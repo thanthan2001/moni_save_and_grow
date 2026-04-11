30dda3f2ce99cd06cbd0558b1cfa1e00db8daae5
thanthan2001 <thanthan101101@gmail.com>
2026-04-11 22:54:53 +0700

    feat: Update import statements and add GEMINI API key in environment configuration

--- lib/features/statistics/presentation/pages/statistics_screen.dart
+++ lib/features/statistics/presentation/pages/statistics_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import '../../../../core/routing/app_back_scope.dart';
 import 'package:fl_chart/fl_chart.dart';
